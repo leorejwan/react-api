b50103bcbbc48e74d87a7dbecf85c403bd3ff180
leo <leorejwan@gmail.com>
2022-02-14 18:06:00 -0300

    Initializing react-app --template typescript. Consuming basic API from github

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+
+type Repositories = {
+  full_name: string;
+  description: string;
+}
+
 
 function App() {
+  const [data, setData] = useState<Repositories[]>([])
+
+  useEffect(() => {
+    fetch('https://api.github.com/users/leorejwan/repos')
+      .then(response =>  response.json())
+      .then(data => {
+        setData(data);
+      })
+  }, [])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ul>
+      {data.map(repo => {
+        return(
+          <li key={repo.full_name}>
+            <strong>{repo.full_name}</strong>
+            <p>{repo.description}</p>
+          </li>
+        )
+      })}
+    </ul>
   );
 }
 
